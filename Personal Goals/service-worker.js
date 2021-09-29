7931604f7ecc05d87eb309cd037d6b8e969e2792
helensousa <57968768+helensousa@users.noreply.github.com>
2021-09-29 10:46:29 -0300

    Add files via upload

---
+++ Personal Goals/service-worker.js
@@ -0,0 +1,46 @@
+var cacheName = 'toferrado-v1.0';
+
+self.addEventListener('install', function (event) {
+  caches.open(cacheName).then((cache) => {
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+      '/css.css',
+      '/css/materialize.css',
+      '/css/materialize.min.css',
+      '/css/style.css',
+      '/js/materialize.js',
+      '/js/materialize.min.js',
+      '/Metas.png',
+      
+    ]);
+  });
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) { 
+  let resposta = caches.open(cacheName).then((cache) => { 
+    return cache.match(event.request).then((recurso) => { 
+      if (recurso) return recurso; 
+      return fetch(event.request).then((recurso) => { 
+        cache.put(event.request, recurso.clone()); 
+        return recurso; 
+      }); 
+    }); 
+  }); 
+  event.respondWith(resposta); 
+});
